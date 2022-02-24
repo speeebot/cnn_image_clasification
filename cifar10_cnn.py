50e47aa7791f30b5dc598101ee7819a55c4644e7
speeebot <speeebot@gmail.com>
2022-02-24 16:34:35 -0500

    pylot code.

--- cifar10_cnn.py
+++ cifar10_cnn.py
@@ -2,20 +2,41 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import sys
 from keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
 
-def getData():
+def get_data():
     (train_x, train_y), (test_x, test_y) = cifar10.load_data()
-    train_x = train_x.astype('float32')
-    test_x = test_x.astype('float32')
+    #normalize to range 0-1
     train_x, test_x = train_x / 255.0, test_x / 255.0
     return train_x, train_y, test_x, test_y
 
+def plot_results(history, metric, val_metric):
+    fig, axs = plt.subplots(2)
+    fig.suptitle('Results')
+    axs[0].plot(history.history[metric])
+    axs[0].plot(history.history[val_metric])
+    axs[0].set_title('model accuracy')
+    axs[0].set(xlabel='epoch', ylabel='accuracy')
+    axs[0].legend(['train', 'test'], loc='upper left')
+    axs[1].plot(history.history['loss'])
+    axs[1].plot(history.history['val_loss'])
+    axs[1].set_title('model loss')
+    axs[1].set(xlabel='epoch', ylabel='loss')
+    axs[1].legend(['train', 'test'], loc='upper left')
+    plt.show()
+
 def main():
-    epochs = 25
+    epochs = 1
     batch_size = 128
+    opt = tf.keras.optimizers.Adam(0.001)
+    loss_fun = 'sparse_categorical_crossentropy'
+    metric = 'sparse_categorical_accuracy'
+    val_metric = 'val_sparse_categorical_accuracy'
 
     # load dataset
-    train_x, train_y, test_x, test_y = getData();
+    train_x, train_y, test_x, test_y = get_data();
+    #train_y = to_categorical(train_y)
+    #test_y = to_categorical(test_y)
 
     # summarize loaded dataset
     print('Train: X=%s, y=%s' % (train_x.shape, train_y.shape))
@@ -23,7 +44,6 @@ def main():
 
     #create model
     model = tf.keras.models.Sequential([
-        #tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu', input_shape = (32, 32, 3)),
         tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu', input_shape = (32, 32, 3)),
         tf.keras.layers.MaxPooling2D(2, 2),
         tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu'),
@@ -31,24 +51,34 @@ def main():
         tf.keras.layers.Conv2D(128, (3, 3), activation = 'relu'),
         tf.keras.layers.Conv2D(256, (3, 3), activation = 'relu'),
         tf.keras.layers.Flatten(),
+        #tf.keras.layers.Dropout(0.2),
+        #tf.keras.layers.Dense(2048, activation = "relu", kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+        #tf.keras.layers.Dropout(0.2),
+        #tf.keras.layers.Dense(2048, activation = "relu", kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+#        tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(2048, activation = "relu"),
+  #      tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(2048, activation = "relu"),
         tf.keras.layers.Dense(10, activation = "softmax")
     ])
 
     #configure model
-    model.compile(optimizer=tf.keras.optimizers.Adam(0.001),
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-                  metrics = [tf.keras.metrics.SparseCategoricalAccuracy()],)
+    model.compile(optimizer=opt, loss=loss_fun, metrics=[metric])
 
     #print out model summary
     model.summary()
 
     #train model
-    model.fit(train_x, train_y, batch_size, epochs)
+    history = model.fit(train_x, train_y, batch_size, epochs, validation_split=0.20)
 
     #evaluate on testing data
-    model.evaluate(test_x, test_y)
+    print("Evaluating on testing data:")
+    test_loss, test_acc = model.evaluate(test_x, test_y)
+
+    print("Test accuracy: {}".format(test_acc))
+
+    #display accuracy over epochs
+    plot_results(history, metric, val_metric)
 
 if __name__ ==  "__main__":
     main()
